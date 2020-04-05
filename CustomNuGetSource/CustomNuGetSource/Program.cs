d9ea265c5980b478cfe9fe9691752fca3132d7e6
Vulcan <vulcan.lee@gmail.com>
2020-04-05 11:16:20 +0800

    在 Visual Studio 內，指定特定的 NuGet 套件安裝來源作法

---
+++ CustomNuGetSource/CustomNuGetSource/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomNuGetSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = Vulcan.ServiceSample.MonkeyData.Monkeys;
+            foreach (var item in foo)
+            {
+                Console.WriteLine($"{item.Name}");
+            }
+
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
