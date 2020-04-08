e4e669751a49033314d966cfbe789281bcb1e21f
Vulcan <vulcan.lee@gmail.com>
2020-04-08 22:06:45 +0800

    Blazor 綁定屬性變更  Part 2 - 實作 PropertyChanged 機制，用來偵測有興趣的物件有變化時，需要作出相對應的處理機制

---
+++ bzINPC/bzINPC/Pages/Person.cs
@@ -0,0 +1,57 @@
+﻿namespace bzINPC.Pages
+{
+    public class Person : System.ComponentModel.INotifyPropertyChanged
+    {
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (age != value)
+                {
+                    age = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
