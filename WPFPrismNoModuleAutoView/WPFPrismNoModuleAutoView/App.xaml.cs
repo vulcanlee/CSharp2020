0ba0bc9efdab6dbfa43a0eccca6dcf251ca21547
Vulcan <vulcan.lee@gmail.com>
2020-04-14 19:38:40 +0800

    WPF Prism 2 - 啟動 WPF 程式之後，在不使用 Module 情況下，如何自動顯示 Region 內的 View

---
+++ WPFPrismNoModuleAutoView/WPFPrismNoModuleAutoView/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using WPFPrismNoModuleAutoView.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+using Prism.Unity;
+using Prism.Regions;
+
+namespace WPFPrismNoModuleAutoView
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        //// 在這裡指定 Region 要顯示的 View ，也是可行的
+        //public override void Initialize()
+        //{
+        //    base.Initialize();
+        //    //IContainerProvider container = (App.Current as PrismApplication).Container;
+        //    IContainerProvider container = Container;
+        //    IRegionManager regionManager = container.Resolve<IRegionManager>();
+        //    regionManager.RegisterViewWithRegion("ContentRegion", typeof(MyView));
+        //}
+    }
+}
---
+++ WPFPrismNoModuleAutoView/WPFPrismNoModuleAutoView/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using WPFPrismNoModuleAutoView.Views;
+
+namespace WPFPrismNoModuleAutoView.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        private readonly IRegionManager regionManager;
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(MyView));
+        }
+    }
+}
