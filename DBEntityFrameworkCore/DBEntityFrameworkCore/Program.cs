3f519b095318b4b07783f26041c55e78f084413b
Vulcan <vulcan.lee@gmail.com>
2020-04-18 17:33:57 +0800

    使用 Console 專案與 Entity Freamework Core 讀取已經存在的 Contoso University 資料庫

---
+++ DBEntityFrameworkCore/DBEntityFrameworkCore/Program.cs
@@ -0,0 +1,17 @@
+﻿using DBEntityFrameworkCore.Models;
+using System;
+
+namespace DBEntityFrameworkCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContosoUniversityContext ContosoUniversityContext = new ContosoUniversityContext();
+            foreach (var item in ContosoUniversityContext.Course)
+            {
+                Console.WriteLine($"{item.Title}");
+            }
+        }
+    }
+}
