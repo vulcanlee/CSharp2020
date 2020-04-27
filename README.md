7c44c19bf484758b65bbdcd4e2e869b048341dac
Vulcan <vulcan.lee@gmail.com>
2020-04-27 20:33:14 +0800

    使用客製命令與轉接器做出一個 CRUD 應用

--- README.md
+++ README.md
@@ -2,15 +2,23 @@
 
 |專案名稱|說明|部落格網址|
 |-|-|-|
+|bzPropertyChangedByJSEvent|Blazor 綁定屬性變更 Part 1 - 透過 DOM 事件機制，用來偵測有興趣的物件有變化時，需要作出相對應的處理機制|https://csharpkh.blogspot.com/2020/04/Blazor-PropertyChanged-Property-Changed-Notification-DOM-Event-oninput-data-binding.html|
+|bzINPC|Blazor 綁定屬性變更 Part 2 - 實作 PropertyChanged 機制，用來偵測有興趣的物件有變化時，需要作出相對應的處理機制|https://csharpkh.blogspot.com/2020/04/Blazor-PropertyChanged-Property-Changed-Notification-INotifyPropertyChanged-data-binding.html|
+|bzINPCbyFody|Blazor 綁定屬性變更 Part 3 - 透過 PropertyChanged.Fody 套件，使用 INotifyPropertyChanged 機制，簡化程式碼設計|https://csharpkh.blogspot.com/2020/04/Blazor-PropertyChanged-Fody-Property-Changed-Notification-INotifyPropertyChanged-data-binding.html|
+|bzSyncfusionDataGrid|在 Blazor 專案內使用 Syncfusion 的 DataGrid 套件 客製化欄位||
+|bzSyncfusionDataGridModel|Blazor 使用 資料模型 來顯示資料表關聯的集合資料 改用非同步方法||
+|bzsfSimpleCRUD|使用 Syncfusion DataGrid 做出一個簡單 CRUD 應用||
+|bzsfCustomBindingCRUD|使用 Syncfusion 轉接器做出一個 CRUD 應用||
+||||
+||||
 |bzsfSimpleCRUD|使用 Syncfusion DataGrid 做出一個簡單 CRUD 應用||
 |bzsfCustomBindingCRUD|使用 Syncfusion 轉接器做出一個 CRUD 應用||
 |bzSidebar|使用 Syncfusion SfSidebar 建立一個版面功能表選項清單||
 |bzSyncfusion|如何使用 Syncfusion 在 Blazor 專案上||
 |bzSyncfusionButton|使用 Syncfusion 的按鈕元件或指定按鈕代表圖形，有 Bootstrap 4 與 Open Iconic 兩種用法||
 |bzSyncfusionDialog|使用 Syncfusion 的各種對話窗的用法，包含巢狀對話窗||
+|bzsfCustomCommandCRUD|使用客製命令與轉接器做出一個 CRUD 應用||
 ||||
-||||
-
 ||||
 ||||
 
---
+++ bzsfCustomCommandCRUD/bzsfSimpleCRUD/Data/OrderService.cs
@@ -0,0 +1,51 @@
+﻿using Syncfusion.Blazor.Navigations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bzsfSimpleCRUD.Data
+{
+    public class OrderService
+    {
+        List<Order> Items { get; set; } = new List<Order>();
+        public OrderService()
+        {
+            Items = Enumerable.Range(1, 75).Select(x => new Order()
+            {
+                OrderID = 1000 + x,
+                CustomerID = (new string[] { "ALFKI", "ANANTR", "ANTON", "BLONP", "BOLID" })[new Random().Next(5)],
+                Freight = 2.1 * x,
+            }).ToList();
+        }
+
+        public Task<IEnumerable<Order>> GetAsync()
+        {
+            return Task.FromResult(Items.AsEnumerable());
+        }
+        public Task<Order> GetAsync(int orderID)
+        {
+            var item = Items.FirstOrDefault(x => x.OrderID == orderID);
+            return Task.FromResult(item);
+        }
+        public Task AddAsync(Order order)
+        {
+            Items.Insert(0,order);
+            return Task.CompletedTask;
+        }
+        public Task UpdateAsync(Order order)
+        {
+            var item =Items.FirstOrDefault(x=>x.OrderID == order.OrderID);
+            item.Freight = order.Freight;
+            item.CustomerID = order.CustomerID;
+
+            return Task.CompletedTask;
+        }
+        public Task DeleteAsync(Order order)
+        {
+            var item = Items.FirstOrDefault(x => x.OrderID == order.OrderID);
+            Items.Remove(item);
+            return Task.CompletedTask;
+        }
+    }
+}
