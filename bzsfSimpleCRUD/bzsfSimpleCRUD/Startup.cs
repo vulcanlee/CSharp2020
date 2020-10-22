45c2a3c43b70d529aca3bbf0a97af3b726d815dc
Vulcan <vulcan.lee@gmail.com>
2020-10-22 20:43:11 +0800

    使用 Syncfusion DataGrid 做出一個具有中文語系的簡單 CRUD 應用

--- bzsfSimpleCRUD/bzsfSimpleCRUD/Startup.cs
+++ bzsfSimpleCRUD/bzsfSimpleCRUD/Startup.cs
@@ -30,43 +30,45 @@ namespace bzsfSimpleCRUD
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            #region 多國語言設定
-            services.AddControllers();
-            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+
+            #region Syncfusion 元件的服務註冊
             services.AddSyncfusionBlazor();
+            #endregion
+
+            #region Localization
+            // Set the resx file folder path to access
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
             // Register the Syncfusion locale service to customize the  SyncfusionBlazor component locale culture
-
             services.AddSingleton(typeof(ISyncfusionStringLocalizer), typeof(SyncfusionLocalizer));
-
             services.Configure<RequestLocalizationOptions>(options =>
             {
-                // define the list of cultures your app will support
+                // Define the list of cultures your app will support
                 var supportedCultures = new List<CultureInfo>()
-                {
-                        new CultureInfo("en-US"),
-                        new CultureInfo("zh-TW")
-                };
-
-                // set the default culture
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("zh-TW")
+            };
+                // Set the default culture
                 options.DefaultRequestCulture = new RequestCulture("zh-TW");
-
                 options.SupportedCultures = supportedCultures;
                 options.SupportedUICultures = supportedCultures;
             });
             #endregion
-
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            #region 多國語言設定
+            #region Localization
             app.UseRequestLocalization(app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>().Value);
             #endregion
 
+            #region 宣告所使用 Syncfusion for Blazor 元件的使用授權碼
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
+            #endregion
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -85,7 +87,6 @@ namespace bzsfSimpleCRUD
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
