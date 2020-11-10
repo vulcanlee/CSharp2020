b785b0a3f323bfe3fbc0dd19a7d33548da1ce60c
Vulcan <vulcan.lee@gmail.com>
2020-11-10 15:19:46 +0800

    C# 平行 / 並行計算 Parallel.For 隱藏在細節背後的惡魔，你所不瞭解的平行與併行計算

---
+++ csParallelFor/csParallelFor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace csParallelFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int processCost = 5000;
+            int N = 100;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Parallel.For(0, N, i =>
+            {
+                Console.WriteLine($"Thread Id:{Thread.CurrentThread.ManagedThreadId}" +
+                    $" , Now={DateTime.Now}");
+                Thread.Sleep(processCost);
+            });
+            stopwatch.Stop();
+            Console.WriteLine($"平行處理 {N} 個工作，共需 {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
